613ee946acf756b61ec0e1a231a3067e2d9667f9
TylerJac <sans.gaming235@gmail.com>
2024-07-22 15:00:30 -0400

    draft 1 of assignment

---
+++ HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String Friend = "Hi" + " Fisher" + " Fletemeyer";
+        System.out.println(Friend);
+    }
+}
---
+++ MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs { public static void main(String[] args){
+        System.out.println("My Name is");
+        System.out.println("Tyler");
+        System.out.println("Jackson");
+    }
+
+}
